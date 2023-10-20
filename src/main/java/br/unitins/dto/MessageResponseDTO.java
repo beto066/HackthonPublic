8007432753ceb0bbcf886203162b4639c9c7a439
danilo <danilo@danilo-VivoBook-ASUSLaptop-X515JA-X515JA.computer.net>
2023-10-20 02:22:35 -0300

    Suport

---
+++ src/main/java/br/unitins/dto/MessageResponseDTO.java
@@ -0,0 +1,18 @@
+package br.unitins.dto;
+
+//import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class MessageResponseDTO{
+    public String text;
+    public String topic;
+/*
+ * String topic
+ * 
+ */
+}
