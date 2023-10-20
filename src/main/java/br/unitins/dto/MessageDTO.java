54ddd08bbe491c0eb0314309e42afa1b3486b6db
beto066 <werbethpereira@unitins.br>
2023-10-20 02:03:44 -0300

    remove entity dto

--- src/main/java/br/unitins/dto/MessageDTO.java
+++ src/main/java/br/unitins/dto/MessageDTO.java
@@ -2,13 +2,13 @@ package br.unitins.dto;
 //import java.util.ArrayList;
 import java.util.List;
 
+import br.unitins.model.DefaultEntity;
 import br.unitins.model.Suport;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
-@Entity
-public class MessageDTO {
+public class MessageDTO{
     public String text;
     public String topic;
     public List<Suport> suport;
