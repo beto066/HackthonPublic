14bfaaa22376de498dcc4d4158b2059c3ddd26bb
danilo <danilo@danilo-VivoBook-ASUSLaptop-X515JA-X515JA.computer.net>
2023-10-20 02:30:44 -0300

    add fields suportDTO

--- src/main/java/br/unitins/dto/SuportDTO.java
+++ src/main/java/br/unitins/dto/SuportDTO.java
@@ -1,11 +1,47 @@
 package br.unitins.dto;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import java.util.List;
 
+import br.unitins.model.Suport;
+
+@Entity 
+
 public class SuportDTO {
-    private MessageDTO message;
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String email;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void toSuport(Suport suport){
+        suport.name = this.name;
+        suport.email  = this.email;
+        suport.senha = this.senha;
+    }
+    //tosuport
 }
